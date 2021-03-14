714895622e409b805662fd660470172aa4bd28d9
Luiz Fernando da Silva <luiz.fernando.tech@gmail.com>
2021-03-14 21:35:48 +0000

    - Adicionado no BaseModelEloquent Before/After (Insert,Update,Delete), para executar alguma coisa necessaria antes ou depois dos comandos. Exemplo: gravas historicos.

--- core/BaseModelEloquent.php
+++ core/BaseModelEloquent.php
@@ -2,11 +2,48 @@
 
 namespace Core;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 require_once __DIR__ . "/bootstrap_eloquent.php";
 
 abstract class BaseModelEloquent extends Model
 {
+    protected function beforeInsert(){}
+
+    protected function afterInsert(){}
+
+    protected function beforeUpdate(){}
+
+    protected function afterUpdate(){}
+
+    protected function beforeDelete(){}
+
+    protected function afterDelete(){}
+
+    protected function performInsert(Builder $query)
+    {
+        $this->beforeInsert();
+        $return = parent::performInsert($query);
+        $this->afterInsert();
+        return $return;
+    }
+
+    protected function performUpdate(Builder $query)
+    {
+        $this->beforeUpdate();
+        $return = parent::performUpdate($query);
+        $this->afterUpdate();
+        return $return;
+    }
+
+    protected function performDeleteOnModel()
+    {
+        $this->beforeDelete();
+        $return = parent::performDeleteOnModel();
+        $this->afterDelete();
+        return $return;
+    }
+
 
 }
